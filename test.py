6eee21c92847813fd97bede8f7c79468d4f46f07
xavysp <xaviersp18@yahoo.com>
2016-08-19 18:09:25 +0200

    Fourier transform coded with> https://www.cs.unm.edu/~brayer/vision/fourier.html

--- test.py
+++ test.py
@@ -3,8 +3,10 @@
 
 import numpy as np
 from scipy.fftpack import fft, ifft
+import cv2
 
 
+# Computing Discrete Fourier Transform
 def computeDft(inreal, inimag):
     assert len(inreal) == len(inimag)
     n = len(inreal)
@@ -22,9 +24,34 @@ def computeDft(inreal, inimag):
     return (outreal, outimag)
 
 
+# Computing Fourier Transform by myself
+def myFt(img):
+    pi = np.pi
+    row, col, depth = np.shape(img)
+    F = np.complex(np.zeros((row,col,depth)))
+    s = np.complex(0)
+    for d in range(depth):
+        for i in range(row):
+            for j in range(col):
+                s = np.complex(0)
+                for i1 in range(row):
+                    for j1 in range(col):
+                        s += img[i1][j1][d]* np.exp(-(np.complex(1))*2*pi*(i*i1+j*j1)/(row*col))
+                F[i][j][d] = s
+    cv2.imshow('Fourier Image', F)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
+# read data
+img = cv2.imread('dataset/1826i.bmp')
+
 data1 = [1, 2, 3, 4]
 data2 = [2, 4, 6, 8]
 
 print (computeDft(data1, data2))
 
+myFt(img)
 
